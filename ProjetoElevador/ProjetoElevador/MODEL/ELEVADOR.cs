d42234fc2888eb76b8e530aa1dde975c4f910e1e
Lucas Flores <lgomesflores74@gmail.com>
2022-03-28 22:09:13 -0400

    atualizando repositório

---
+++ ProjetoElevador/ProjetoElevador/MODEL/ELEVADOR.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProjetoElevador.MODEL
+{
+    public class ELEVADOR
+    {
+        private int CAPMAX;     //CAPACIDADE MÁXIMA//
+        private int TOTFLOOR;   //TOTAL DE DE ANDARES//
+        private int QUANTP=0;   //QUANTIDADE DE PESSOAS//
+        private int FLOOR = 0;  //ANDAR//
+
+        public void Inicializar(int CAP, int FLOORS){
+            this.CAPMAX = CAP;
+            this. TOTFLOOR = FLOORS;
+        }
+
+        public void Entrar()
+        {
+
+            if (this.QUANTP == this.CAPMAX) {
+                Console.WriteLine("Capacidade Máxima Atingida! Por Favor, Aguarde!");
+            } else if (this.QUANTP < this.CAPMAX){
+                this.QUANTP += 1;
+                Console.WriteLine($"Quantidade de Pessoas no Elevador:{this.QUANTP}");
+            }
+        }
+        public void Sair(){
+            if (this.QUANTP == 0)
+            {
+                Console.WriteLine("Elevador Vazio!");
+            } else if (this.QUANTP > 0)
+            {
+                this.QUANTP -= 1;
+                Console.WriteLine($"Quantidade de Pessoas no Elevador: {this.QUANTP}");
+            }
+        }
+        public void Subir()
+        {
+        if (this.FLOOR == this.TOTFLOOR)
+            {
+                Console.WriteLine("Limite Atingido");
+            }else { this.FLOOR+=1;
+                Console.WriteLine($"Andar: {this.FLOOR}");
+            }
+        }
+        public void Descer()
+        {
+            if (this.FLOOR == 0)
+            {
+                Console.WriteLine("Térreo.");
+            }
+            else
+            {
+                this.FLOOR -= 1;
+                if (this.FLOOR == 0)
+                {
+                    Console.WriteLine("Térreo.");
+                }
+                else
+                {
+                    Console.WriteLine($"Andar: {this.FLOOR}");
+                }
+
+            }
+
+
+        }
+
+     }
+}
---
+++ ProjetoElevador/ProjetoElevador/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+using ProjetoElevador.MODEL;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+
+            ELEVADOR UPDOWN = new ELEVADOR();
+            int CAP = 0;
+            int TOTFLOOR = 0;
+            int COMANDO = 0;
+
+
+            Console.WriteLine("Atenção!Elevador entrando em Operação!");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("Definir Capacidade do elevador");
+            CAP = int.Parse(Console.ReadLine());
+            Console.WriteLine("Definir Total de Andares do Edificio");
+            TOTFLOOR = int.Parse(Console.ReadLine());
+
+            UPDOWN.Inicializar(CAP,TOTFLOOR);
+            Console.Clear();
+
+            bool continuar = true;
+
+           
+            do
+            {
+                Console.WriteLine(@" Escolha uma opção:
+                                     1 - Entrando
+                                     2 - Saindo
+                                     3 - Subindo
+                                     4 - Descendo
+                                     5 - Sair");
+
+                string TAKE = Console.ReadLine();
+                Console.Clear();
+
+                switch (TAKE)
+                {
+                    case "1":
+                        Console.WriteLine("Entrar");
+                        UPDOWN.Entrar();
+                        break;
+                    case "2":
+                        Console.WriteLine("Sair");
+                        UPDOWN.Sair();
+                        break;
+                    case "3":
+                        Console.WriteLine("Subir");
+                        UPDOWN.Subir();
+                        break;
+                    case "4":
+                        UPDOWN.Descer();
+                        Console.WriteLine("Descer");
+                        break;
+                    case "5":
+                        continuar = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("");
+                        break;
+                }
+            }
+
+            while (continuar);          
+
+
+        }
+    }
+}
